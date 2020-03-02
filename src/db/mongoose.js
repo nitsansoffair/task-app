4d9db37891e502571bfe0cad5aefd1ddeebfa22b
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-02 21:01:43 +0200

    Resource Creation Endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,15 +5,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/tasks-app-api', {
     useCreateIndex: true,
     useUnifiedTopology: true
 });
-
-const Task = new mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
