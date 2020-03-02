772bbfc5e384cb6be5fc441b1a30d8f23c471af4
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-03 01:56:23 +0200

    Resource deleting endpoints

--- src/index.js
+++ src/index.js
@@ -84,6 +84,24 @@ app.patch('/users/:id', async(req, res) => {
     }
 });
 
+app.delete('/users/:id', async(req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if(!user){
+            res
+                .status(404)
+                .send();
+        }
+
+        res.send(user);
+    }catch (e) {
+        res
+            .status(500)
+            .send();
+    }
+});
+
 app.post('/tasks', async(req, res) => {
     const task = new Task(req.body);
 
@@ -157,6 +175,24 @@ app.patch('/tasks/:id', async(req, res) => {
         res
             .status(400)
             .send(e);
+    }
+});
+
+app.delete('/tasks/:id', async(req, res) => {
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if(!task){
+            res
+                .status(404)
+                .send();
+        }
+
+        res.send(task);
+    }catch (e) {
+        res
+            .status(500)
+            .send();
     }
 });
 
