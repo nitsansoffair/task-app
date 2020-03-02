9b446df35d53dab199319caa8addbeb685949a9b
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-02 20:54:24 +0200

    Resource Creation Endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,63 +1,10 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/tasks-app-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
-
-// const User = new mongoose.model('User', {
-//     name: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         lowercase: true,
-//         validate(value){
-//             if(!validator.isEmail(value)){
-//                 throw new Error('Email is invalid');
-//             }
-//         }
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         validate(value){
-//             if(value.toLowerCase().indexOf('password') !== -1){
-//                 throw new Error('Password should not contain `password`');
-//             }
-//         },
-//         trim: true,
-//         minlength: 7
-//     },
-//     age: {
-//         type: Number,
-//         default: 0,
-//         validate(value){
-//             if(value < 0){
-//                 throw new Error('Age must be a positive number')
-//             }
-//         }
-//     }
-// });
-//
-// const me = new User({
-//     name: '   Andrew  ',
-//     email: 'EMAIL@MEAD.IO   '
-// });
-//
-// me.save()
-//     .then(() => {
-//         console.log(me);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     });
 
 const Task = new mongoose.model('Task', {
     description: {
@@ -70,11 +17,3 @@ const Task = new mongoose.model('Task', {
         default: false
     }
 });
-
-const task = new Task({
-    description: '   description   '
-});
-
-task.save()
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
