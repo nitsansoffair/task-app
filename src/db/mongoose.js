98144531bedf3c61dce5b54a9516a5ad2047b467
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-02 19:59:49 +0200

    Creating a Mongoose model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,24 +6,42 @@ mongoose.connect('mongodb://127.0.0.1:27017/tasks-app-api', {
     useUnifiedTopology: true
 });
 
-const User = new mongoose.model('User', {
-    name: {
+// const User = new mongoose.model('User', {
+//     name: {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     }
+// });
+//
+// const me = new User({
+//     name: 'Andrew',
+//     age: 'Mike'
+// });
+//
+// me.save()
+//     .then(() => {
+//         console.log(me);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+const Task = new mongoose.model('Task', {
+    description: {
         type: String
     },
-    age: {
-        type: Number
+    completed: {
+        type: Boolean
     }
 });
 
-const me = new User({
-    name: 'Andrew',
-    age: 'Mike'
+const task = new Task({
+    description: 'description',
+    completed: false
 });
 
-me.save()
-    .then(() => {
-        console.log(me);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+task.save()
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
