4c3c5115eba877646646d911cb01757280f0a22f
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-04 01:21:14 +0200

    Hiding private data

--- src/index.js
+++ src/index.js
@@ -6,20 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if(req.method === 'GET'){
-//         res.send('GET requests are disabled');
-//     }else{
-//         next();
-//     }
-// });
-
-// app.use((req, res, next) => {
-//     res
-//         .status(503)
-//         .send('Middleware');
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -27,15 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
-
-const jwt = require('jsonwebtoken');
-
-const myFunction = async() => {
-    const token = jwt.sign({ _id: 'aaa' }, '111', { expiresIn: '7 days' });
-    console.log(token);
-
-    const data = jwt.verify(token, '111');
-    console.log(data);
-};
-
-myFunction();
