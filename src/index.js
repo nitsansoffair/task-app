a025c6f3225ed9c7a9f0cc14b0ab07616e407eb2
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-05 03:12:15 +0200

    Code styling

--- src/index.js
+++ src/index.js
@@ -13,19 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-    // const task = await Task.findById('5e5ef639c5965e036c76dde6');
-    // await task.populate('owner').execPopulate();
-    // console.log(task.owner);
-
-    const user = await User.findById('5e5ef5b2d00a921d880fd315');
-    await user.populate('tasks').execPopulate();
-
-    console.log(user.tasks);
-};
-
-main();
