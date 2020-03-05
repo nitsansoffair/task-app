f6318b0ff32782cd14a1b4c3132b6dc43a39712d
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-06 01:30:56 +0200

    Adding images to user profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,20 +4,6 @@ const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
 
-const upload = multer({
-    dest: 'avatars',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb){
-        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
-            return cb(new Error('File format must be a jpg or jpeg or png'));
-        }
-
-        cb(undefined, true);
-    }
-});
-
 router.post('/users', async(req, res) => {
     const user = new User(req.body);
 
@@ -117,7 +103,23 @@ router.delete('/users/me', auth, async(req, res) => {
     }
 });
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('File format must be a jpg or jpeg or png'));
+        }
+
+        cb(undefined, true);
+    }
+});
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async(req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+
     res.send();
 }, (err, req, res, next) => {
     res
@@ -125,6 +127,14 @@ router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
         .send({
             error: err.message
         });
+});
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+
+    res
+        .send();
 });
 
 module.exports = router;
