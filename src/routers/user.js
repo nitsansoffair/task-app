9cd8241833101009e2eb4866637d46cc404dc834
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-05 23:58:38 +0200

    Adding support for uploading files

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
+
+const upload = multer({
+    dest: 'avatars'
+});
 
 router.post('/users', async(req, res) => {
     const user = new User(req.body);
@@ -100,6 +105,10 @@ router.delete('/users/me', auth, async(req, res) => {
             .status(500)
             .send();
     }
+});
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send();
 });
 
 module.exports = router;
