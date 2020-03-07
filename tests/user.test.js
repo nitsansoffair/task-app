fed8a7e796ed62e4c00feaf5b86a5f12791b6bf1
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-07 18:11:21 +0200

    Wrapping up user tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -101,3 +101,39 @@ test('Should not delete account for unauthenticated user', async () => {
         .send()
         .expect(401);
 });
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'update_1'
+        })
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+
+    expect(user.name).toBe('update_1');
+});
+
+test('Should bot update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'update_1'
+        })
+        .expect(400);
+});
