a3cfc3e89e892aab2dcb918a841fd847ac585cfe
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-04 00:14:12 +0300

    Add GraphQL query

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+app.use('/graphql', graphqlHttp(({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver
+})));
 
 module.exports = app;
