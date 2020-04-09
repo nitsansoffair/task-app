8f991786fcde9aab52e9ee57792bd0d1f60a788b
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-10 02:01:34 +0300

    add new test and describe

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,13 +2,26 @@ const { expect } = require('chai');
 
 const authMiddleware = require('../src/middleware/auth');
 
-it('Should throw an error if no authorization header is present', async () => {
-    const req = {
-        header: function () {
-            return null;
-        }
-    };
+describe('Auth middleware', function () {
+    it('Should set req.isAuth to false if no authorization header is present', async () => {
+        const req = {
+            header: function () {
+                return null;
+            }
+        };
 
-    await authMiddleware(req, {}, () => {});
-    expect(req.isAuth).to.equal(false);
+        await authMiddleware(req, {}, () => {});
+        expect(req.isAuth).to.equal(false);
+    });
+
+    it('Shoudk set req.isAuth to false if the authorization header is only one string', async () => {
+        const req = {
+            header: function () {
+                return 'Bearer';
+            }
+        };
+
+        await authMiddleware(req, {}, () => {});
+        expect(req.isAuth).to.equal(false);
+    });
 });
