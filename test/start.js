645dda3fc19045d0b7faab3ddea100ddaace44fc
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-10 01:40:06 +0300

    testing with Mocha and chai

---
+++ test/start.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+
+it('Should add numbers correctly', () => {
+    const num1 = 2;
+    const num2 = 3;
+    const res = num1 + num2;
+
+    expect(res).to.equal(5);
+});
+
+it('Should not give a result of 6', () => {
+    const num1 = 2;
+    const num2 = 3;
+    const res = num1 + num2;
+
+    expect(res).not.to.equal(6);
+});
