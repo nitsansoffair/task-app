10a0ef67f20ccb686fbc6be22b23787a2cea56e2
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-10 17:47:19 +0300

    Use Compression for compress html, css, javascript, and Helmet for secure http headers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
+const helmet = require('helmet');
+const compression = require('compression');
 
 require('./db/mongoose');
 const userRouter = require('./routers/user');
@@ -9,6 +11,9 @@ const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
 
 const app = express();
+
+app.use(helmet());
+app.use(compression());
 
 app.use(express.json());
 app.use(userRouter);
