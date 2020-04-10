427e5e90031f4f1bf511c7737980f3074105ddcb
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-10 20:49:58 +0300

    using ssl keys for security

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
+const https = require('https');
+
 const app = require('./app');
 const logger = require('./logger');
 
+const privateKey = fs.readFileSync('./src/ssl/key.pem');
+const certificate = fs.readFileSync('./src/ssl/server.crt');
+
 const port = process.env.PORT;
 
-app.listen(port, () => {
-    logger.info(`Server is up on port ${port}`);
-});
+https
+    .createServer({ key: privateKey, cert: certificate }, app)
+    .listen(port, () => {
+        logger.info(`Server is up on port ${port}`);
+    });
