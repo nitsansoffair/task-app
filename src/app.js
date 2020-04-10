a5b3e91b6ff1f5bad415bb25343bd529581b51b4
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-10 17:57:05 +0300

    Use morgan for logging

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const graphqlHttp = require('express-graphql');
 const helmet = require('helmet');
 const compression = require('compression');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 require('./db/mongoose');
 const userRouter = require('./routers/user');
@@ -11,9 +14,14 @@ const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
 
 const app = express();
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'),
+    { flags: 'a' }
+    );
 
 app.use(helmet());
 app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(express.json());
 app.use(userRouter);
